bc9a384367eb22191e98a1f7826c8bbc6b589130
Chris21-lkb <77533476+Chris21-lkb@users.noreply.github.com>
2021-04-08 19:38:51 +0800

    commit

--- resources/views/pages/Trainers.blade.php
+++ resources/views/pages/Trainers.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.layout1')
 @section('content')
-<div class="trainers-section-contact">
-    <div class="trainers_picture">
-        @foreach($infos as $info)
-        <div class="trainers-picture-info">
-            <img src="{{asset('/storage/trainerPictures/' .$info->file_path)}}" alt="Image">
-            <div class="trainer_name">
-                <h3>{{ $info['coachName']}}</h3>
-                <p>GYM TRAINER</p>
-            </div>
-        </div>
-        @endforeach
+<div class="trainers-page-here">
+    <div class="trainers-section-title">
+        <h1>TRAINERS</h1>
     </div>
+    <div class="trainers-section-contact">
+        <div class="trainers_picture">
+            @foreach($infos as $info)
+            <div class="trainers-picture-info">
+                <img src="{{asset('/storage/trainerPictures/' .$info->file_path)}}" alt="Image">
+                <div class="trainer_name">
+                    <a href="#"><h3>{{ $info['coachName']}}</h3></a>
+                    <p>GYM TRAINER</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
 
-    <div class="trainersPageAdds">
-        <h1>Adds here </h1>
+        <div class="trainersPageAdds">
+            <h1>Adds here </h1>
+        </div>
     </div>
 </div>
 @stop
