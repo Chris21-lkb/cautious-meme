b00f1d1e61c3bb04a53295682d32c7cb1cb74239
Chris21-lkb <77533476+Chris21-lkb@users.noreply.github.com>
2021-04-08 20:02:56 +0800

    commit

--- resources/views/includes/head1.blade.php
+++ resources/views/includes/head1.blade.php
@@ -2,3 +2,5 @@
 <meta name="description" content="">
 <title> HOME PAGE </title>
 <link rel="stylesheet" type="text/css" href="{{asset('/css/style1.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('/css/footer.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('/css/header.css')}}">
