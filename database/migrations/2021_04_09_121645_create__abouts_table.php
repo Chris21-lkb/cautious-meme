4727d5fb2dc1dfc26a375923c090592e46e03fe4
Chris21-lkb <77533476+Chris21-lkb@users.noreply.github.com>
2021-04-09 20:19:55 +0800

    commit

---
+++ database/migrations/2021_04_09_121645_create__abouts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->string('address');
+            $table->string('picture_path');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Abouts');
+    }
+}
